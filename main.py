e9695db285db4575eb348212442f2e2b409c9ecd
zhur123 <44005653+zhur123@users.noreply.github.com>
2022-11-25 20:55:31 -0500

    Cleaned up code

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def initialize():
 
 def find_best(test_image):
     diff_list = []
-    start = time()
+    # start = time()
     test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
     for (card_name, gt_image) in gt_images:
         gt_image = cv2.cvtColor(gt_image, cv2.COLOR_BGR2GRAY)
@@ -46,13 +46,18 @@ def find_best(test_image):
     # print('Comparing with other cards took', time()-start, 'seconds') # Takes around 60 seconds
     diff_list.sort()
     return diff_list
-    # best = (np.inf, '')
-    # for (card_name, gt_image) in gt_images:
-    #     diff = np.abs(gt_image - test_image).sum()
-    #     rotate_diff = np.abs(cv2.rotate(gt_image, cv2.ROTATE_180) - test_image).sum()
-    #     if rotate_diff < best[0] or diff < best[0]:
-    #         best = (diff if diff < rotate_diff else rotate_diff, card_name)
-    # return best
+
+def test(image_name):
+    initialize()
+    image_path = os.path.join(test_path, image_name)
+    test_image = crop.crop_image(image_path, goal_dim=goal_dim)
+    diff_list = find_best(test_image)
+    percent = (diff_list[1][0] - diff_list[0][0]) / diff_list[1][0]
+    print(image_name, 'is predicted to be', diff_list[0][1], 'with a difference of', diff_list[0][0], str(round(percent*100)) + '%')
+    if percent <= 0.5:
+        print('The next 5 possibilities are: ')
+        for i in range(1, 6):
+            print(diff_list[i])
 
 def main():
     initialize()
@@ -67,34 +72,10 @@ def main():
             for i in range(1, 6):
                 print(diff_list[i])
 
-def test(image_name):
-    initialize()
-    image_path = os.path.join(test_path, image_name)
-    test_image = crop.crop_image(image_path, goal_dim=goal_dim)
-    diff_list = find_best(test_image)
-    percent = (diff_list[1][0] - diff_list[0][0]) / diff_list[1][0]
-    print(image_name, 'is predicted to be', diff_list[0][1], 'with a difference of', diff_list[0][0], str(round(percent*100)) + '%')
-    if percent <= 0.5:
-        print('The next 5 possibilities are: ')
-        for i in range(1, 6):
-            print(diff_list[i])
-        
-
 if __name__ == '__main__':
     main()
     # test('Test6.jpg')
-    # cv2.imshow('Image', test_image)
-    # test_image = cv2.resize(cv2.cvtColor(crop.crop_image('./Test_Images/Test1.jpg'), cv2.COLOR_BGR2GRAY), (700, 500))
-    # test_image = crop.crop_image('./Test_Images/Test1.jpg').astype('int')
-    # test_image = crop.crop_image('./Test_Images/Test1.jpg')
 
-    # gt_image_cropped = cv2.resize(cv2.cvtColor(cv2.imread('./Ground_Truth_Cropped/2_hearts.jpg'), cv2.COLOR_BGR2GRAY), (700, 500))
-    # gt_image_cropped = cv2.imread('./Ground_Truth_Cropped/2_hearts.jpg').astype('int')
-    # gt_image_cropped = cv2.imread('./Ground_Truth_Cropped/2_hearts.jpg')
     # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     # cv2.imshow('image', cv2.resize(test_image, (700, 500)))
     # cv2.waitKey(0)
-    # print('diff')
-    # print(test_image.astype('int')[0, 3, 0] - gt_image_cropped[0, 3, 0].astype('int'))
-    # print((np.array(test_image, int) - np.array(gt_image_cropped, int)).sum())
-    # print(np.abs(test_image - gt_image_cropped).sum())
